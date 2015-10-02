7edea29215c000713f9fd5a14d7243a6f4134215
David Green <david.lindsay.green@gmail.com>
2015-10-02 11:26:28 -0400

    Added interp1 implementatoin

--- ADAS/ionization.m
+++ ADAS/ionization.m
@@ -1,7 +1,7 @@
 %function z = ionization(z,dt,n,T,State,RateCoeff)
 
 z=1;
-T=15;
+T=750;
 dt=1e-4;
 n=1e11;
 
@@ -17,7 +17,13 @@ end
 
 frac = (T-Te(i-1,z))/(Te(i,z)-Te(i-1,z));
 
-Coeff = RateCoeff(i-1,z) + frac*RateCoeff(i,z);
+Coeff = RateCoeff(i-1,z) + frac*RateCoeff(i,z)
+
+Coeff2 = interp1(Te(:,z),RateCoeff(:,z),T)
+
+if ( isnan(Coeff2) ) 
+    error('Ionization interpolation out of range')
+end
 
 tion = 1/(Coeff*n)
 P1 = exp(-dt/tion)
