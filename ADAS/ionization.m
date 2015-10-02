c210f754bfcbb4b8945ed8045eef2f52a59bcb69
Tim Younkin <tyounkin@vols.utk.edu>
2015-10-02 17:10:05 -0400

    Corrected interpolation

--- ADAS/ionization.m
+++ ADAS/ionization.m
@@ -1,13 +1,16 @@
 %function z = ionization(z,dt,n,T,State,RateCoeff)
+%Syntax for creating multiple random number streams
+
+[s1,s2,s3] = RandStream.create('mrg32k3a','NumStreams',3); %Include ,'Seed','shuffle' to get different values each time
 
 z=1;
 T=750;
-dt=1e-4;
-n=1e11;
+dt=1e-9;
+n=1e15;
 
 Nelem = size(Te(:,z));
 diff = Te(:,z)-T;
-
+%{
 for i=1:Nelem
     if (diff(i) >=0)
         i;
@@ -15,21 +18,24 @@ for i=1:Nelem
     end
 end
 
-frac = (T-Te(i-1,z))/(Te(i,z)-Te(i-1,z));
+frac = (T-Te(i-1,z))/(Te(i,z)-Te(i-1,z))
 
-Coeff = RateCoeff(i-1,z) + frac*RateCoeff(i,z)
+Coeff = RateCoeff(i-1,z) + frac*(RateCoeff(i,z) - RateCoeff(i-1,z))
+%}
+Coeff = interp1(Te(:,z),RateCoeff(:,z),T)
 
-Coeff2 = interp1(Te(:,z),RateCoeff(:,z),T)
-
-if ( isnan(Coeff2) ) 
+if ( isnan(Coeff) ) 
     error('Ionization interpolation out of range')
 end
 
 tion = 1/(Coeff*n)
-P1 = exp(-dt/tion)
+P1 = 1-exp(-dt/tion)
 
-
- if rand >= P1
+r1=rand(s1)
+ if r1 <= P1
     z = z+1
-end   
+ end   
+
+r1 = rand(s1)
+r1 = rand(s1)
 
