5bc991c20fe235dfe697cd3adebebea5e87a350c
Tim Younkin <tyounkin@vols.utk.edu>
2016-02-09 13:40:51 -0500

    deleted directories

--- matlab/Utilities/perpDist.m
+++
@@ -1,3 +0,0 @@
-function perpDistanceToSurface = perpDist(xp,surface_dz_dx,surface_zIntercept)
-perpDistanceToSurface = ( -surface_dz_dx*xp(1) + xp(3) + surface_zIntercept)/sqrt(surface_dz_dx^2+1);
-end
