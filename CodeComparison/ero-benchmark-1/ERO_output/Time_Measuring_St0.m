1baaad311e03d66e6509d99149d64b4196688384
Ane Lasa Esquisabel <a7l@siesta.ornl.gov>
2016-02-16 11:42:12 -0500

    adding ERO output for Reference case

---
+++ CodeComparison/ero-benchmark-1/ERO_output/Time_Measuring_St0.m
@@ -0,0 +1,10 @@
+%============================================================
+% Created by ERO (Standard Version)
+% for the task 'Time_Measuring'.
+% Wed Feb 10 10:58:10 2016
+%============================================================
+%
+TskName= 'Time_Measuring';
+CompName= 'STD';
+%
+main =    332.0000;
---
+++ CodeComparison/ero-benchmark-1/ERO_output/Wtile_test_Ctrl_St0.m
@@ -0,0 +1,66 @@
+%============================================================
+% Created by ERO (Standard Version)
+% for the task 'Wtile_test_'.
+% Wed Feb 10 10:52:37 2016
+%============================================================
+%
+TskName= 'Wtile_test_';
+CompName= 'STD';
+%
+%
+NLstEl = 1;
+ListOfElem = [
+ '_w';
+];
+%
+%
+NMSComb = 1;
+
+NComb = 2;
+Combiantions = [
+ '_d_w';
+ '_w_w';
+];
+%
+% Wed Feb 10 10:52:37 2016
+% time needed to allocate spectroscopy:
+% 0.000000 seconds
+
+%------------------------------
+
+% Wed Feb 10 10:52:37 2016
+% time needed to allocate surface grid:
+% 0.000000 seconds
+
+%------------------------------
+
+% Wed Feb 10 10:52:41 2016
+% time needed to calculate concentrations:
+% 4.000000 seconds
+
+%------------------------------
+
+% Wed Feb 10 10:57:59 2016
+% time of physical source loop:
+% 317424.651123 milliseconds
+
+%------------------------------
+
+% Wed Feb 10 10:57:59 2016
+ % CYCLE of NORMAL SOURCE:
+ 316.877943
+
+%------------------------------
+
+% Wed Feb 10 10:57:59 2016
+% time needed to calculate source cycle:
+% 317.000000 seconds
+
+%------------------------------
+
+% Wed Feb 10 10:58:09 2016
+% time needed for info output:
+% 10.000000 seconds
+
+%------------------------------
+
