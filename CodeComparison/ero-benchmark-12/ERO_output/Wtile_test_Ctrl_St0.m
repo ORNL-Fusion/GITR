8fb7a3109e586b9fa8dde64e14c7618916b9b2f9
Ane Lasa Esquisabel <a7l@siesta.ornl.gov>
2016-02-24 17:06:52 -0500

    rename Dperp and vflow cases, add the NP and gridsize cases

---
+++ CodeComparison/ero-benchmark-12/ERO_output/Wtile_test_Ctrl_St0.m
@@ -0,0 +1,66 @@
+%============================================================
+% Created by ERO (Standard Version)
+% for the task 'Wtile_test_'.
+% Wed Feb 24 14:47:58 2016
+%============================================================
+%
+TskName= 'Wtile_test_';
+CompName= 'STD';
+%
+%
+NLstEl = 1;
+ListOfElem = [
+ '_w';
+];
+%
+%
+NMSComb = 1;
+
+NComb = 2;
+Combiantions = [
+ '_d_w';
+ '_w_w';
+];
+%
+% Wed Feb 24 14:47:59 2016
+% time needed to allocate spectroscopy:
+% 1.000000 seconds
+
+%------------------------------
+
+% Wed Feb 24 14:47:59 2016
+% time needed to allocate surface grid:
+% 0.000000 seconds
+
+%------------------------------
+
+% Wed Feb 24 14:48:03 2016
+% time needed to calculate concentrations:
+% 4.000000 seconds
+
+%------------------------------
+
+% Wed Feb 24 14:54:09 2016
+% time of physical source loop:
+% 365555.240967 milliseconds
+
+%------------------------------
+
+% Wed Feb 24 14:54:09 2016
+ % CYCLE of NORMAL SOURCE:
+ 365.323243
+
+%------------------------------
+
+% Wed Feb 24 14:54:09 2016
+% time needed to calculate source cycle:
+% 366.000000 seconds
+
+%------------------------------
+
+% Wed Feb 24 14:54:19 2016
+% time needed for info output:
+% 10.000000 seconds
+
+%------------------------------
+
---
+++ CodeComparison/ero-benchmark-3/ERO_output/Wtile_test_Ctrl_St0.m
@@ -0,0 +1,66 @@
+%============================================================
+% Created by ERO (Standard Version)
+% for the task 'Wtile_test_'.
+% Wed Feb 24 14:30:42 2016
+%============================================================
+%
+TskName= 'Wtile_test_';
+CompName= 'STD';
+%
+%
+NLstEl = 1;
+ListOfElem = [
+ '_w';
+];
+%
+%
+NMSComb = 1;
+
+NComb = 2;
+Combiantions = [
+ '_d_w';
+ '_w_w';
+];
+%
+% Wed Feb 24 14:30:43 2016
+% time needed to allocate spectroscopy:
+% 1.000000 seconds
+
+%------------------------------
+
+% Wed Feb 24 14:30:43 2016
+% time needed to allocate surface grid:
+% 0.000000 seconds
+
+%------------------------------
+
+% Wed Feb 24 14:30:47 2016
+% time needed to calculate concentrations:
+% 4.000000 seconds
+
+%------------------------------
+
+% Wed Feb 24 15:31:35 2016
+% time of physical source loop:
+% 3647526.907959 milliseconds
+
+%------------------------------
+
+% Wed Feb 24 15:31:35 2016
+ % CYCLE of NORMAL SOURCE:
+ 3646.795571
+
+%------------------------------
+
+% Wed Feb 24 15:31:35 2016
+% time needed to calculate source cycle:
+% 3648.000000 seconds
+
+%------------------------------
+
+% Wed Feb 24 15:31:46 2016
+% time needed for info output:
+% 11.000000 seconds
+
+%------------------------------
+
