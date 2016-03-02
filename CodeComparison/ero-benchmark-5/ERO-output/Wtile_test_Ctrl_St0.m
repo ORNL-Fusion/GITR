f9d14b980a56dc8b6ba1112223d945b95eb3ec76
Ane Lasa Esquisabel <a7l@siesta.ornl.gov>
2016-03-02 11:59:31 -0500

    adding ERO's output for benchmark-5 (dt=1e-8)

---
+++ CodeComparison/ero-benchmark-5/ERO-output/Wtile_test_Ctrl_St0.m
@@ -0,0 +1,66 @@
+%============================================================
+% Created by ERO (Standard Version)
+% for the task 'Wtile_test_'.
+% Tue Mar  1 16:31:36 2016
+%============================================================
+%
+TskName= 'Wtile_test_';
+CompName= 'STD';
+%
+%
+NLstEl = 1;
+ListOfElem = [
+ '_w';
+];
+%
+%
+NMSComb = 1;
+
+NComb = 2;
+Combiantions = [
+ '_d_w';
+ '_w_w';
+];
+%
+% Tue Mar  1 16:31:37 2016
+% time needed to allocate spectroscopy:
+% 1.000000 seconds
+
+%------------------------------
+
+% Tue Mar  1 16:31:37 2016
+% time needed to allocate surface grid:
+% 0.000000 seconds
+
+%------------------------------
+
+% Tue Mar  1 16:31:41 2016
+% time needed to calculate concentrations:
+% 4.000000 seconds
+
+%------------------------------
+
+% Tue Mar  1 16:33:50 2016
+% time of physical source loop:
+% 128862.426025 milliseconds
+
+%------------------------------
+
+% Tue Mar  1 16:33:50 2016
+ % CYCLE of NORMAL SOURCE:
+ 128.830880
+
+%------------------------------
+
+% Tue Mar  1 16:33:50 2016
+% time needed to calculate source cycle:
+% 129.000000 seconds
+
+%------------------------------
+
+% Tue Mar  1 16:34:43 2016
+% time needed for info output:
+% 53.000000 seconds
+
+%------------------------------
+
