9337bee8f0b769020af8c5b4b1412b3183b5769d
Ane Lasa Esquisabel <a7l@siesta.ornl.gov>
2016-03-02 12:16:32 -0500

    adding ERO's output for benchmark-6 (dt=1e-9)

---
+++ CodeComparison/ero-benchmark-6/ERO-output/Time_Measuring_St0.m
@@ -0,0 +1,10 @@
+%============================================================
+% Created by ERO (Standard Version)
+% for the task 'Time_Measuring'.
+% Tue Mar  1 16:54:46 2016
+%============================================================
+%
+TskName= 'Time_Measuring';
+CompName= 'STD';
+%
+main =   1311.0000;
