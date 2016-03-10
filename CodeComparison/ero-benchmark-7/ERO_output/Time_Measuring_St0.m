900775495732223b72591e454e8dc8f4ef85a1be
Ane Lasa Esquisabel <a7l@siesta.ornl.gov>
2016-03-10 17:55:24 -0500

    benchmark 7: NO collisions and fixed dt=1e-9

---
+++ CodeComparison/ero-benchmark-7/ERO_output/Time_Measuring_St0.m
@@ -0,0 +1,10 @@
+%============================================================
+% Created by ERO (Standard Version)
+% for the task 'Time_Measuring'.
+% Wed Mar  9 22:19:31 2016
+%============================================================
+%
+TskName= 'Time_Measuring';
+CompName= 'STD';
+%
+main =   1242.0000;
