47bcd3d061541aa4941480e6e1135e8cd26c50f5
Tim Younkin <tyounkin@vols.utk.edu>
2016-03-21 10:59:05 -0400

    fixed cpp boris integrator

--- cpp/src/Particle.cpp
+++ cpp/src/Particle.cpp
@@ -17,21 +17,34 @@ void Particle::BorisMove(double dt,double xMinV,double xMaxV,double yMin,double 
 	double r[3] = {0, 0, 0};
 	double surfaceDirection[3] = {1.7321, 0, -1.00};//{1.4281, 0, -1.00};//{2.1445, 0, -1.00};//{1.7321, 0, -1.00};
 	double surfaceDirection_unit[3] = {0.866, 0, -0.50};//{0.9063, 0, -0.4226}; // {0.866, 0, -0.50};
-	double perpd;
+	double t;
+	double surface_dz_dx;
+	
+	surface_dz_dx = surfaceDirection[0];
 	
 
 	perpDistanceToSurface = ( -surfaceDirection[0]*x + z )/2.0;//1.7434;// 2.0 2.3662;
+	//std::cout << "Perp dist" << perpDistanceToSurface << std::endl;
+	//std::cout << " Particle position in xyz " << x << " " << y << " " << z << std::endl;
 	//perpd = ( -0.5773*x + z )/1.1547;
 Efield( E, perpDistanceToSurface);
 	
 	double Bmag = 2;
 	
 	           double     q_prime = Z*1.60217662e-19/(amu*1.6737236e-27)*dt*0.5;
-              double  coeff = 2*q_prime/(1+(q_prime*Bmag)*(q_prime*Bmag));	
+              double  coeff = 2*q_prime/(1+(q_prime*Bmag)*(q_prime*Bmag));
               
-              				  v_minus[0] = vx + q_prime*E[0];
-                              v_minus[1] = vy + q_prime*E[1];
-                              v_minus[2] = vz + q_prime*E[2];
+              v[0] = vx;
+              v[1] = vy;
+              v[2] = vz;
+              
+              r[0] = x;
+              r[1] = y;
+              r[2] = z;	
+              
+              				  v_minus[0] = v[0] + q_prime*E[0];
+                              v_minus[1] = v[1] + q_prime*E[1];
+                              v_minus[2] = v[2] + q_prime*E[2];
                 
                 v[0] = v_minus[0] + q_prime*(v_minus[1]*B[2] - v_minus[2]*B[1]);
                 v[1] = v_minus[1] + q_prime*(v_minus[2]*B[0] - v_minus[0]*B[2]);
@@ -41,24 +54,37 @@ Efield( E, perpDistanceToSurface);
                 v[1] = v_minus[1] + coeff*(v[2]*B[0] - v[0]*B[2]);
                 v[2] = v_minus[2] + coeff*(v[0]*B[1] - v[1]*B[0]);
                 
-                vx = v[0] + q_prime*E[0];
-                vy = v[1] + q_prime*E[1];
-                vz = v[2] + q_prime*E[2];
+                v[0] = v[0] + q_prime*E[0];
+                v[1] = v[1] + q_prime*E[1];
+                v[2] = v[2] + q_prime*E[2];
                 
 
                 r[0] = x + v[0]*dt;
                 r[1] = y + v[1]*dt;
                 r[2] = z + v[2]*dt;
                 
+                perpDistanceToSurface = ( -surfaceDirection[0]*r[0] + r[2] )/2.0;
                 
-                			if (r[0] > xMinV && r[0] < xMaxV && r[1] > yMin && r[1] < yMax
-			&& r[2] > zMin && r[2] < zMax)
+                			if (perpDistanceToSurface < 0)
+			{
+			            t = (surface_dz_dx*x - z)/(-surface_dz_dx*(x -r[0]) + (z -r[2]));
+                        x = x + (x - r[0])*t;
+                        y = y + (y - r[1])*t;
+                        z = z + (z - r[2])*t;
+
+			}
+			else
 			{
 			x = r[0];
 			y = r[1];
 			z = r[2];
 			}
-   // std::cout << "e: " << E[0] << std::endl;
+			
+			vx = v[0];
+			vy = v[1];
+			vz = v[2];
+//    std::cout << "Pos: " << x << y << z << std::endl;
+	
 }
 
 void Particle::Ionization(double dt)
@@ -74,10 +100,16 @@ void Particle::Ionization(double dt)
 	
 	double r1=((double)rand()/(double)RAND_MAX);
 	
+	if( Z == 0)
+	{ P1 = 1.0;
+	}
+	else P1 = 0.0;
+	
 	if(r1 <= P1)
 	{
 		Z = Z+1;
 		//std::cout << "ionization" << std::endl;
+		//std::cout << " Particle position in xyz " << x << " " << y << " " << z << std::endl;
 	} 
 	
 	//std::cout << "P1 and r1" << P1 << r1 << std::endl;
