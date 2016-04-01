733ad956c5d33415c593d4b650842e25dfe0e14b
David Green <david.lindsay.green@gmail.com>
2016-04-01 12:34:36 -0400

    Thrust speed tests working. Please verify thrust calculation with CPU calculation

--- cpp/cuda/include/cudaParticle.h
+++ cpp/cuda/include/cudaParticle.h
@@ -10,35 +10,41 @@
 
 class cudaParticle {
 	public:
-		double x;
-		double y;
-		double z;
-      		double vx;
-      		double vy;
-      		double vz;
-      		double Z;
-      		double amu;
+		float x;
+		float y;
+		float z;
+      	float vx;
+      	float vy;
+      	float vz;
+      	float Z;
+      	float amu;
+        float dt;
 
 		__host__ __device__ 
         cudaParticle() {
             x=0.0;
-	    y=0.0;
-	    z=0.0;
+	        y=0.0;
+	        z=0.0;
         };
 		__host__ __device__
-        cudaParticle(double x,double y, double z, double Ex, double Ey, double Ez, double Z, double amu){
-               this->x = x;
-               this->y = y;
-               this->z = z;
-	       this->Z = Z;
-		this->amu = amu;
-		this->vx = Ex/fabs(Ex)*sqrt(2.0*fabs(Ex)*1.60217662e-19/(amu*1.6737236e-27));
-		this->vy = Ey/fabs(Ey)*sqrt(2.0*fabs(Ey)*1.60217662e-19/(amu*1.6737236e-27));
-		this->vz = Ez/fabs(Ez)*sqrt(2.0*fabs(Ez)*1.60217662e-19/(amu*1.6737236e-27));
-		
-		if(Ex == 0.0) this->vx = 0.0;
-		if(Ey == 0.0) this->vy = 0.0;
-		if(Ez == 0.0) this->vz = 0.0; 
+        cudaParticle(float x,float y, float z, float Ex, float Ey, float Ez, float Z, float amu, float dt){
+    
+            this->dt = dt;
+
+            this->x = x;
+            this->y = y;
+            this->z = z;
+
+	        this->Z = Z;
+		    this->amu = amu;
+
+		    this->vx = Ex/fabs(Ex)*sqrt(2.0*fabs(Ex)*1.60217662e-19/(amu*1.6737236e-27));
+		    this->vy = Ey/fabs(Ey)*sqrt(2.0*fabs(Ey)*1.60217662e-19/(amu*1.6737236e-27));
+		    this->vz = Ez/fabs(Ez)*sqrt(2.0*fabs(Ez)*1.60217662e-19/(amu*1.6737236e-27));
+		    
+		    if(Ex == 0.0) this->vx = 0.0;
+		    if(Ey == 0.0) this->vy = 0.0;
+		    if(Ez == 0.0) this->vz = 0.0; 
         };
 };
 
