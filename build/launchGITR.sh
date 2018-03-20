589cef277423ae91bdad1cc4f09aa1554554d212
Timothy Younkin <tyounkin@vols.utk.edu>
2018-03-19 18:13:46 -0700

    updated surface model for cpu version and added mpi timers

--- build/launchGITR.sh
+++ build/launchGITR.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Running shell script for GITR mpirun"
-mpirun -n 2 /project/projectdirs/atom/atom-install-edison/GITR/build/GITR
-#srun -n 1 -c 24 $GITR_PATH/build/GITR
+#mpirun -n 2 /project/projectdirs/atom/atom-install-edison/GITR/build/GITR
+srun -n 10 -c 24 $GITR_PATH/build/GITR
 exit 0
