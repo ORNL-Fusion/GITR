139d15eeb79da2f4745976fce9af968a0495d857
Timothy Younkin <tyounkin@vols.utk.edu>
2018-05-29 13:30:08 -0700

    removed output2d being done in parallel causing hang

--- python/gitr.py
+++ python/gitr.py
@@ -543,8 +543,10 @@ def plotPitch(filename='positions.nc'):
 if __name__ == "__main__":
     #asdfanc_show("surface.nc")
     #depositedEdist()
-    nc_plotHist('output/history.nc')
-    nc_plotSpec('output/spec.nc')
+    if(os.path.exists('output/history.nc')):
+    	nc_plotHist('output/history.nc')
+    if(os.path.exists('output/spec.nc')):
+    	nc_plotSpec('output/spec.nc')
     iter2dProcessing()
     #nc_plotSpec3D()
     #nc_plotPositions()
