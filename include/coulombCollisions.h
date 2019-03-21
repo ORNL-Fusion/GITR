cf1e27d7ac8828804fc789fc49320d7a3eb93696
Timothy Younkin <tyounkin@Timothys-MacBook-Pro.local>
2019-03-21 12:50:09 -0400

    update for immediate thermalization of particles

--- include/coulombCollisions.h
+++ include/coulombCollisions.h
@@ -537,6 +537,14 @@ void operator()(std::size_t indx)  {
         float density = interp2dCombined(x,y,z,nR_Dens,nZ_Dens,DensGridr,DensGridz,ni);
 	float tau_s = particlesPointer->amu[indx]*ti_eV*sqrt(ti_eV/background_amu)/(6.84e4*(1.0+background_amu/particlesPointer->amu[indx])*density/1.0e18*particlesPointer->charge[indx]*particlesPointer->charge[indx]*15);
 	//std::cout << "ti dens tau_s " << ti_eV << " " << density << " " << tau_s << endl;
+	float vTherm = sqrt(ti_eV*1.602e-19/particlesPointer->amu[indx]/1.66e-27);
+
+                float vxy00 = sqrt(vTherm*vTherm - vz*vz);
+                float vxy01 = sqrt(vx*vx + vy*vy);
+		//std::cout << "vzNew vxy0 vxy " << vzNew << " " << vxy0 << " " << vxy << endl;
+                particlesPointer->vx[indx] = vx/vxy01*vxy00;///velocityCollisionsNorm; 
+		particlesPointer->vy[indx] = vy/vxy01*vxy00;///velocityCollisionsNorm;
+		//particlesPointer->vz[indx] = vzNew;///velocityCollisionsNorm;   	
         float drag = -dt*nu_friction*velocityRelativeNorm;
 	//std::cout << "nu_friction " << nu_friction << std::endl;
 	//std::cout << "vrelative " << velocityRelativeNorm << std::endl;
