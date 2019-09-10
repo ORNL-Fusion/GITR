cb3bafdc2639d40fec3d4fee66064cf69a588121
Tim Younkin <tyounkin19@gmail.com>
2019-09-10 14:14:24 -0400

    updated cfg testing to include strings

--- test/file_io_tests.cpp
+++ test/file_io_tests.cpp
@@ -39,4 +39,36 @@ TEST_CASE("Factorials are computed", "[factorial]") {
     REQUIRE_THAT(this_thing,
                      Catch::Matchers::WithinAbs(0.12345, tol));
   }
+  SECTION("double")
+  {
+    std::cout << "starting test " << std::endl;
+    typedef double P;
+    libconfig::Config cfg;
+    std::string file = "../test/file.cfg";
+    std::cout << "about to importLibConfig " << std::endl;
+    importLibConfig(cfg, file);
+    std::cout << "imported cfg " << std::endl;
+    const std::string var1 = "stuff.float";
+    double this_thing=0;
+    getVariable(cfg, var1,this_thing);
+    std::cout << "this thing values " << this_thing << std::endl;
+    double tol = 1e-3;
+    REQUIRE_THAT(this_thing,
+                     Catch::Matchers::WithinAbs(0.12345, tol));
+  }
+  SECTION("string")
+  {
+    std::cout << "starting test " << std::endl;
+    typedef double P;
+    libconfig::Config cfg;
+    std::string file = "../test/file.cfg";
+    std::cout << "about to importLibConfig " << std::endl;
+    importLibConfig(cfg, file);
+    std::cout << "imported cfg " << std::endl;
+    const std::string var1 = "stuff.filename";
+    std::string this_thing;
+    getVariable(cfg, var1,this_thing);
+    std::cout << "this thing values " << this_thing << std::endl;
+    REQUIRE(this_thing == "netcdf_file_py.nc");
+  }
 }
