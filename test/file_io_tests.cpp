7b8dd35856f711ecef94722a682dec3201dd2b2f
Tim Younkin <tyounkin19@gmail.com>
2019-09-09 23:12:23 -0400

    Updated CMakeLists to link used libraries in test and added basic test of getVariable

--- test/file_io_tests.cpp
+++ test/file_io_tests.cpp
@@ -3,19 +3,20 @@
 #include <iostream>
 #include <libconfig.h++>
 #include <stdio.h>
-
+#include "utils.h"
 #include "catch.hpp"
 
 TEST_CASE("Factorials are computed", "[factorial]") {
-  //std::cout << "starting test " << std::endl;
-  //typedef double P;
-  //libconfig::Config cfg;
-  //std::string file = "file.cfg";
-  //std::cout << "about to importLibConfig " << std::endl;
-  //importLibConfig(cfg, file);
-  //std::cout << "imported cfg " << std::endl;
-  //const std::string var1 = "stuff.thing";
-  int this_thing = 1; // getVariable<int>(cfg, var1);
+  std::cout << "starting test " << std::endl;
+  typedef double P;
+  libconfig::Config cfg;
+  std::string file = "../test/file.cfg";
+  std::cout << "about to importLibConfig " << std::endl;
+  importLibConfig(cfg, file);
+  std::cout << "imported cfg " << std::endl;
+  const std::string var1 = "stuff.thing";
+  int this_thing=0;
+  getVariable(cfg, var1,this_thing);
   std::cout << "this thing values " << this_thing << std::endl;
   REQUIRE(this_thing == 1);
 }
