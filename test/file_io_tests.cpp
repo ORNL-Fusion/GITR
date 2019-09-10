579648fd553c574875891ba40cdaff6dd31447e9
Tim Younkin <tyounkin19@gmail.com>
2019-09-10 11:43:08 -0400

    moved template of getVariable to cpp file and added approx test for getVariable float

--- test/file_io_tests.cpp
+++ test/file_io_tests.cpp
@@ -7,16 +7,36 @@
 #include "catch.hpp"
 
 TEST_CASE("Factorials are computed", "[factorial]") {
-  std::cout << "starting test " << std::endl;
-  typedef double P;
-  libconfig::Config cfg;
-  std::string file = "../test/file.cfg";
-  std::cout << "about to importLibConfig " << std::endl;
-  importLibConfig(cfg, file);
-  std::cout << "imported cfg " << std::endl;
-  const std::string var1 = "stuff.thing";
-  int this_thing=0;
-  getVariable(cfg, var1,this_thing);
-  std::cout << "this thing values " << this_thing << std::endl;
-  REQUIRE(this_thing == 1);
+  SECTION("int")
+  {
+    std::cout << "starting test " << std::endl;
+    typedef double P;
+    libconfig::Config cfg;
+    std::string file = "../test/file.cfg";
+    std::cout << "about to importLibConfig " << std::endl;
+    importLibConfig(cfg, file);
+    std::cout << "imported cfg " << std::endl;
+    const std::string var1 = "stuff.thing";
+    int this_thing=0;
+    getVariable(cfg, var1,this_thing);
+    std::cout << "this thing values " << this_thing << std::endl;
+    REQUIRE(this_thing == 1);
+  }
+  SECTION("float")
+  {
+    std::cout << "starting test " << std::endl;
+    typedef double P;
+    libconfig::Config cfg;
+    std::string file = "../test/file.cfg";
+    std::cout << "about to importLibConfig " << std::endl;
+    importLibConfig(cfg, file);
+    std::cout << "imported cfg " << std::endl;
+    const std::string var1 = "stuff.float";
+    float this_thing=0;
+    getVariable(cfg, var1,this_thing);
+    std::cout << "this thing values " << this_thing << std::endl;
+    float tol = 1e-3;
+    REQUIRE_THAT(this_thing,
+                     Catch::Matchers::WithinAbs(0.12345, tol));
+  }
 }
