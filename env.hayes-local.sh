1cbe784f0c0681f87573cde3528da9f8f0aca9ad
Alyssa Hayes <hayes@utk.edu>
2019-10-01 10:44:07 -0400

    added local install files

---
+++ env.hayes-local.sh
@@ -0,0 +1,13 @@
+export LIBCONFIGPP_INCLUDE=/usr/local/include
+export THRUST_INCLUDE_DIRS=$HOME/Dev/thrust
+export THRUST_DIR=$HOME/Dev/thrust
+export PATH=/usr/local/Cellar/netcdf/4.6.1_1/:$PATH
+export  NETCDF_DIR=/usr/local
+export  NETCDFCXX_DIR=/usr/local
+export  NETCDFDIR=$NETCDF_DIR/lib
+export  NETCDFCXX4DIR=$NETCDFCXX_DIR/lib
+export  NETCDFINCLUDE=$NETCDF_DIR/include
+export  NETCDFCXX4INCLUDE=$NETCDFCXX4_DIR/include
+export  NETCDFLIB=$NETCDFDIR/libnetcdf.dylib
+export  NETCDFLIB_CPP=$NETCDFCXX4DIR/libnetcdf-cxx4.dylib
+export PYTHONPATH=$HOME/Dev/gitr2/python:$PYTHONPATH
