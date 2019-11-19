fcedbe1b4ca681c91ab961ede22728d376010799
Tim Younkin <tyounkin19@gmail.com>
2019-11-19 16:05:25 -0500

    made shell commands

--- action-build/entrypoint.sh
+++ action-build/entrypoint.sh
@@ -2,8 +2,8 @@
 
 sh -c "echo directory $PWD"
 sh -c "ls /home/runner/work/GITR-1/GITR-1"
-cd GITR-1/build
+sh -c "cd build"
 sh -c "echo now directory $PWD"
 sh -c "ls /github/workspace"
 
-./makeGITR.sh
+sh -c "./makeGITR.sh"
