e4143b3df23cce13a1eb877a516ddd09ed24620f
Tim Younkin <tyounkin19@gmail.com>
2019-11-19 16:00:47 -0500

    ls

--- action-build/entrypoint.sh
+++ action-build/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -l
 
 sh -c "echo directory $PWD"
-
+sh -c "ls /home/runner/work/GITR-1/GITR-1"
 cd GITR-1/build
 sh -c "echo now directory $PWD"
+sh -c "ls /github/workspace"
 
 ./makeGITR.sh
