5fd430d1babcb507e22f0cb50ff98c2d03b55d58
Tim Younkin <tyounkin19@gmail.com>
2019-11-20 01:15:07 -0500

    netcdf bin

--- action-dependency/entrypoint.sh
+++ action-dependency/entrypoint.sh
@@ -6,5 +6,6 @@ apt-get -y update
 sh -c "echo updated tree"
 apt-get -y install libnetcdf-dev
 sh -c "echo netcdf stuff"
+apt-get -y install netcdf-bin
 sh -c "nc-config --libdir"
 sh -c "nc-config --includedir"
