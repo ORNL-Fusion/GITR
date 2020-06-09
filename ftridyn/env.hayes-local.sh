384b8b5c9f53efa339d76a69bfa8603941ed1254
Alyssa Hayes <hayes@utk.edu>
2020-06-09 10:52:18 -0400

    updates from previous workflow

---
+++ ftridyn/env.hayes-local.sh
@@ -0,0 +1,8 @@
+#export PYTHONPATH=/Users/tyounkin/Code/mpi4pyBuild/lib.macosx-10.12-x86_64-2.7/:$PYTHONPATH
+#export PYTHONPATH=/Users/tyounkin/Code/numpyBuild-1.14.3/lib.macosx-10.12-x86_64-2.7:$PYTHONPATH
+export PYTHONPATH=/Users/Alyssa/Dev/fractal-tridyn/utils/:$PYTHONPATH
+export PYTHONPATH=/Users/Alyssa/Dev/fractal-tridyn/src/:$PYTHONPATH
+export PYTHONPATH=/Users/Alyssa/Dev/GITR/ftridyn/:$PYTHONPATH
+#export PYTHONPATH=$HOME/Code/FtridynFromPython/build:$PYTHONPATH
+
+
