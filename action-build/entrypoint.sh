42bc473172b0130a62669e716f25df95ec72d178
Tim Younkin <tyounkin19@gmail.com>
2020-08-14 23:26:40 -0400

    added action folders back

---
+++ action-build/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -l
+
+sh -c "echo directory $PWD"
+sh -c "ls /home/runner/work/GITR-1/GITR-1"
+sh -c "cd build"
+sh -c "echo now directory $PWD"
+sh -c "ls /github/workspace"
+
+sh -c "build/makeGITRactions.sh"
