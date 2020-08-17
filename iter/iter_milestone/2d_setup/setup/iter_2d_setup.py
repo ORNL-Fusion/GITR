71b156c7770ab473be41e63293800491ddfa4936
Tim Younkin <tyounkin19@gmail.com>
2020-08-17 13:49:31 -0400

    updates to iter_2d_setup python to run complete setup

--- iter/iter_milestone/2d_setup/setup/iter_2d_setup.py
+++ iter/iter_milestone/2d_setup/setup/iter_2d_setup.py
@@ -24,13 +24,13 @@ solps.make_solps_targ_file(gitr_geom_filename='gitr_geometry.cfg', \
      solps_geom = 'assets/b2fgmtry', \
      right_target_filename= 'assets/rightTargOutput')
 
-solps.make_solps_targ_file_txt(solps_geom='assets/b2fgmtry',b_field_file = 'assets/Baseline2008-li0.70.x4.equ')
+solps.make_solps_targ_file_txt(solps_geom='assets/b2fgmtry',b_field_file = 'assets/Baseline2008-li0.70.x4.equ',coords_file = 'right_target_coordinates.txt', right_target_filename= 'assets/rightTargOutput')
 
 hpic.plot_hpic_iead(solps_path='solpsTarg.txt', \
         HpicDataFolder = ['assets/hPIC_IEAD_solps_conditions/hPIC_IEAD_DATA', \
         'assets/hPIC_IEAD_He2_final/hPIC_IEAD_He2'],\
         solpsIndex = [3,4])
-hpic.computeSputtYld(plots=False,hpic_zipfile='hpic_ieads',solps_inds = [3,4],nEdistPts=40)
+hpic.computeSputtYld(plots=False,hpic_zipfile='hpic_ieads',solps_inds = [3,4],nEdistPts=40,ftridyn_background_file = 'assets/ftridynBackground.nc')
 
 gitrParticleSource.particleSource2d(nParticles = int(1e3),spylFile = 'Yields.txt', coordsFile='right_target_coordinates.txt',edist_file = 'Edists.txt',adist_file = 'Adists.txt')
 shutil.copyfile('particleSource.nc', '../input/particleSource.nc')
