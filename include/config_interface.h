6a9889b31bfd918ef833824a5b7ae331edb7bf4c
null <null>
2021-05-18 23:23:18 -0400

    intermediate state of new configuration interface
    does not currently compile. This is the skeleton of the
    new configuration interface.

--- include/config_interface.h
+++ include/config_interface.h
@@ -1,31 +1,112 @@
 #include <iostream>
 #include <string>
+#include <memory>
 #include "libconfig.h++"
 
-/*
-Could I make a const class, const constructed? Can a const class have virtual data members?
-You should have const accessors to mutable data instead. This class is the flattener. It will
-provide 
-*/
-class libconfig_data_reader
-{
-};
-
-/* Inherits from a class that defines a mapping etc? Subclass to work with the data reader */
-/* The base class should work with the API that the config_interface class will use */
-/* subclass once and simply define a constructor that sets the variables a certain way */
-/* Include a map of < scoped enum > : config_module that can return a sub-config module */
-/* overload the get() method based on what type of scoped enum it is called with */
-/* this effectively embeds the type information into the scoped enum string calls */
-/* does it effectively add type to an int?? Yep, that's the whole point in fact */
-class config_module
-{
-};
-
-/* Creates a list of config_module that can be queried for objects */
-class config_interface
+class libconfig_string_query
 {
   public:
 
+  libconfig_string_query( std::string libconfig_file );
+
+  template< typename T >
+  void operator()( std::string const query_key, T &query_value ) const
+  {
+    bool success = cfg.lookupValue( query_key, query_value );
+
+    if( success == false ) 
+    {
+      std::cout << "invalid query key: " << query_key << std::endl;
+      exit(0);
+    }
+  }
+
+  private:
+
+  libconfig::Config cfg;
+};
+
+class config_module_base
+{
+  public:
+
+  config_module_base( std::string module_path = "" );
+
+  std::string const &module_path() { return module_path }
+
+  /* get config sub-module */
+  virtual std::shared_ptr< config_module_base > get( int key );
+  /* get a config value */
+  virtual void get( int key, double &val );
+  virtual void get( int key, float &val );
+  virtual void get( int key, int &val );
+  virtual void get( int key, std::string &val );
+
+  private:
+
+  std::string module_path;
+};
+
+class impurity_particle_source final : public config_module_base
+{
+  public:
+
+  /* get config sub-module */
+  std::shared_ptr< config_module_base > get( int key );
+
+  /* get a config value */
+  void get( int key, double &val ) override;
+  void get( int key, float &val ) override;
+  void get( int key, int &val ) override;
+  void get( int key, std::string &val ) override;
+  /* define strongly typed enums as well - set them to int? */
+
+  private:
+
+  class libconfig_string_query const &query;
+};
+
+class gitr_config_interface : public config_module_base
+{
+  public:
+
+  /* define the mapping */
+  gitr_config_interface();
+
+  /* define scoped enums to refer to the various config modules */
+
   std::string ahoy() const;
 };
+
+/*
+
+Ok... how about instead of that... we just put the root setting in there?
+
+Goals: separate libconfig from this. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
