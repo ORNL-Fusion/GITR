ef65753f3683c13aea4e583fbce0e223af9c4cc2
null <null>
2021-09-26 08:22:00 -0400

    Linear interpolation verified for up to 6 dimensions. Needs refactor.

--- include/interpolator.h
+++ include/interpolator.h
@@ -8,6 +8,8 @@ double interpolate_basis_cell( std::vector< double > const &basis_cell,
                                std::vector< int > const &d_len,
                                std::vector< double > const &max_values )
 {
+  std::cout << "Ahoy, Captain! Inside interpolate_basis_cell" << std::endl;
+
   assert( coordinates.size() == d_len.size() );
 
   assert( coordinates.size() == max_values.size() );
@@ -17,10 +19,14 @@ double interpolate_basis_cell( std::vector< double > const &basis_cell,
   for( int i = 0; i < spacing.size(); i++ )
   {
     spacing[ i ] = max_values[ i ] / double(d_len[ i ]);
+    std::cout << "spacing[ i ] = " << spacing[ i ] << std::endl;
   }
+  std::cout << std::endl;
 
   /* each basis component will have 2 numbers associated with it */
   std::vector< double > normalized_fractions( coordinates.size() * 2 );
+
+  std::cout << "fractions:" << std::endl;
 
   for( int i = 0; i < coordinates.size(); i++ )
   {
@@ -29,28 +35,57 @@ double interpolate_basis_cell( std::vector< double > const &basis_cell,
     ( coordinates[ i ] - ( std::floor( coordinates[ i ] / spacing[ i ] ) * spacing[ i ] ) ) /
     spacing[ i ];
 
+    /* show the calculation of the fractions */
+    std::cout << "coordinates[ " << i << " ] = " << coordinates[ i ]
+              << " base_point[ " << i << " ] = "  
+              << ( std::floor( coordinates[ i ] / spacing[ i ] ) * spacing[ i ] )
+              << " difference = " 
+              << ( coordinates[ i ] - ( std::floor( coordinates[ i ] / spacing[ i ] ) 
+                   * spacing[ i ] ) )
+              << std::endl;
+
     /* low fraction, matches with a 0 bit */
     normalized_fractions[ i * 2 ] = 1 - normalized_fractions[ i * 2 + 1 ];
+
+    std::cout << "low " << i << " = " << normalized_fractions[ i * 2 ]
+              << " high " << i << " = " << normalized_fractions[ i * 2 + 1 ] << std::endl;
   }
+  std::cout << std::endl;
 
   /* we have the basis cell, what else is needed? */
   double sum = 0;
+
   for( int i = 0; i < basis_cell.size(); i++ )
   {
     double product = 1;
 
+    std::cout << "product on term " << i << ":" << std::endl;
+
     /* iterate the bits in "i" */
-    for( int j = 0; j < d_len.size(); j++ )
+    for( int j = 0; j < coordinates.size(); j++ )
     {
+      int index = j * 2 + ( ( i & ( 0x1 << j ) ) >> j );
+
+      std::string which = ( index % 2 ? "high" : "low" );
+
+      std::cout << which << "*";
+
       /* Captain! Should I calculate instead of store? */
+      /* Captain! Do i >> j & 0x1 instead for a shorter expression, save a clock cycle */
       product *= normalized_fractions[ j * 2 + ( ( i & ( 0x1 << j ) ) >> j ) ];
     }
+    std::cout << std::endl;
+    std::cout << "product = " << product << std::endl;
+    std::cout << "basis_cell[ " << i << " ] = " << basis_cell[ i ] << std::endl;
 
     sum += product * basis_cell[ i ];
+
+    std::cout << "sum = " << sum << std::endl << std::endl;
   }
 
   return sum;
 }
+
 /*
 
 What do you need to do next? Next, we need to figure out how to make more tests.
--- test_src/interpolator_tests.cpp
+++ test_src/interpolator_tests.cpp
@@ -1,13 +1,53 @@
 #include "test_utils.hpp"
 #include "interpolator.h"
 
-/* build basis cell, adapt to interpolate it */
-std::vector< double > generate_basis_cell( int len, int d )
+/*
+
+s = cell_dimension
+d = dimension of interest
+fraction = domain and range fraction
+low = lower bound bin value
+high = upper bound bin value
+
+*/
+std::vector< double > generate_basis_cell_1( int s, 
+                                             int d,
+                                             double fraction,
+                                             double low,
+                                             double high )
 {
+  std::vector< double > basis_cell( 1 << s );
+
+  std::cout << "generate_basis_cell_1:" << std::endl;
+  std::cout << s << "dimensional-space split across dimension " << d << std::endl;
+
+  for( int i = 0; i < basis_cell.size(); i++ )
+  {
+    /* if d is 0, use the low. If it is 1, use the high */
+    if( ( i >> d ) & 0x1 ) basis_cell[ i ] = high ;
+
+    else basis_cell[ i ] = low;
+
+    std::cout << "basis_cell[ " << i << " ] = " << basis_cell[ i ] << std::endl;
+  }
+
+  std::cout << std::endl;
+
+  return basis_cell;
+}
+
+/* Ahoy! Build a new basis cell */
+std::vector< double > generate_basis_cell_0( int len, int d )
+{
+  int test = 8;
+  int most_significant = ( ( test & ( 0x1 << 3 ) ) >> 3  );
+  std::cout << "Ahoy, Captain! most_significant bit: " << most_significant << std::endl;
+
   std::vector< double > v( 1 << d );
 
   double len_squared = len * len;
 
+  /* In what order does this interpolate stuff? */
   for( int i = 0; i < 1 << d; i++ )
   {
     double sum = 0;
@@ -91,91 +131,161 @@ std::vector< double > embed_basis_cell( std::vector< int > const &d_len,
   return haystack;
 }
 
-/* The tests should form a circle back to a known answer:
-
-generate_basis_cell with known interpolation characteristics <---> interpolate_basis_cell
-
-*/
-
 /* Captain! A row-major, flat data layout is assumed. Note that. */
 TEST_CASE( "n-dimensional interpolation" )
 {
   /* see if embed and fetch are truly inverses like you think */
   SECTION( "embed and fetch a basis cell" )
   {
+    /* Captain! Next, make this multidimensional */
     int basis_len = 10;
 
     int dims = 3;
 
-    auto v0 = generate_basis_cell( basis_len, dims );
+    auto v0 = generate_basis_cell_0( basis_len, dims );
 
-    /* points in each dim */
+    /* points in each dim - the space is stretched out so the math remains the same
+       as if it was a uniform grid space */
+       /* these are the cell dimensions of the space */
     std::vector< int > d_len{ 6, 4, 10 };
 
-    /* this should result in a spacing of 2 for each dimension */
+    /* this should result in a spacing of 2 in the domain for each dimension */
+    /* these are the float space x domain values */
     std::vector< double > max_values{ 12, 8, 20 };
 
     /* coordinates where it goes */
+    /* target location in cell indices */
     std::vector< int > target_index_tuple{ 3, 2, 5 };
 
     /* the interpolation set for this should be 8 points starting with the one above */
-    std::vector< double > coordinates{ 6.9, 4.9, 10.9 };
+    /* target location cell indices element wise multiplied with spacing plus some 
+       arbitrary offset */
+    /* Captain! Express coordinates in terms of the variables in the comment above */
+    //std::vector< double > coordinates{ 6.9, 4.9, 10.9 };
+    std::vector< double > coordinates{ 7.8, 5.8, 11.8 };
+
+    std::vector< double > spacing( 3 );
+
+    for( int i = 0; i < spacing.size(); i++ )
+    {
+      spacing[ i ] = max_values[ i ] / double(d_len[ i ]);
+    }
 
     auto data = embed_basis_cell( d_len,
                                   target_index_tuple,
                                   v0 );
 
-    /* Captain! You now need float coordinates that you know will map to the coordinates above */
     auto v1 = fetch_basis_cell( data,
                                 coordinates, 
                                 d_len,
                                 max_values );
 
-    /* pick a diagonal point in this basis cell. It will be some fraction of the farthest
-       away point, the top vertex. Interpolate that point. It should give you a value
-       that represents a fraction of the farthest away point's distance. */
-
-    /* alright. You gotta big brain this. */
-
-    /* get this to work before you do the interpolations */
     REQUIRE( v0 == v1 );
 
-    /* 
-       The coordinates of the interpolated point: The must be: 
+    double uniform_fraction = 0.5;
 
-       base_index + spacing[ i ]
+    /* Ahoy! This is the part of the test that does not work */
+    /*
+    std::vector< double > test_point( coordinates.size() );
 
-       The value of the interpolated point:
+    std::cout << "Captain! End of the test:" << std::endl;
+
+    for( int i = 0; i < test_point.size(); i++ )
+    {
+      test_point[ i ] = 
+      ( double( target_index_tuple[ i ] ) + uniform_fraction ) * spacing[ i ];
+      std::cout << "test_point[ " << i << " ] = " << test_point[ i ] << std::endl;
+    }
+
+    double correct_value = uniform_fraction * v1.back();
+
+    double interpolated_value = interpolate_basis_cell( v1, test_point, d_len, max_values );
+
+    REQUIRE( correct_value == interpolated_value );
     */
   }
 
-  /* this is the hypotenuse test, relies on the geometric identity */
-  SECTION( "interpolate an nd basis cell" )
+  SECTION( "interpolate basis cell" )
   {
-    int basis_len = 10;
+    /* Captain! Declare these parameters */
+    double fraction = 0.6;
 
-    int dims = 3;
+    double low = 10;
 
-    auto basis_cell = generate_basis_cell( basis_len, dims );
+    double high = 20;
 
-    /* points in each dim */
-    std::vector< int > d_len{ 6, 4, 10 };
+    /* the domain spacing value - space in between domain bins */
+    double spacing = 8;
 
-    /* this should result in a spacing of 2 for each dimension */
-    std::vector< double > max_values{ 12, 8, 20 };
+    /* number of cells that span dimension d, assuming the start domain value is 0 */
+    double d_len = 5;
 
-    /* the interpolation set for this should be 8 points starting with the one above */
-    std::vector< double > coordinates{ 6.9, 4.9, 10.9 };
+    /* note: this is the maximum domain value */
+    double max_value = d_len * spacing;
 
-    /* you now need... an nd interpolator */
-    double i = interpolate_basis_cell( basis_cell );
-  }
+    double gold = ( 1 - fraction ) * low + fraction * high;
+    
+    /* create a domain value that maps to the gold interpolated value */
+    /* put the point in the final cell */
+    double test_domain_value = ( d_len - 1 ) * spacing + fraction * spacing;
 
-  /* for this test, create a 4d dataset showing a time series progression of
-     a 3d field. Make sure the interpolation places the value in the cell center. Just
-     add to all values in the cube? */
-  SECTION( "interpolate a 4d dataset, cell center, time average of two values." )
-  {
+    /* number of co-planar test points - only variation is within the norm of the plane dirction
+       so all interpolations of points in this plane should produce the same answer */
+    int n_test_points = 5; 
+
+    int initial_dim = 1;
+
+    int final_dim = 6;
+
+    for( int s = initial_dim; s < final_dim; s++ )
+    {
+      std::cout << "dimension: " << s << std::endl;
+
+      /* make the 1d test template constants into n-dimensional vectors for interpolations */
+      /* later, make a set of test points with dimension d's domain value being the test
+         but vary the other dimensional coordinates to be anywhere in the cell. Set element
+         "d" below to the test_domain_value and vary the others. Set them all the same then
+         just set the "d" element to test_domain_value in the loop */
+      std::vector< double > const test_point( s, test_domain_value );
+      std::vector< int > const d_len_s( s, d_len );
+      std::vector< double > const max_value_s( s, max_value );
+
+      /* iterate the bits in a point in s-dimensional space */
+      /* assume zyx is the bit representation order */
+      for( int d = 0; d < s; d++ )
+      {
+        std::vector< double > const basis_cell =
+        generate_basis_cell_1( s, d, fraction, low, high );
+
+
+        /*
+
+        each test point will have values of
+        ( d_len - 1 ) * spacing + i / n_test_points * spacing 
+        for i going from 1 to n_test_points - 1.
+
+        Add this dimension to the loop - you should get the same
+        answer. Verify correct behavior, then clean up. Also put in a variadic template test case
+
+        */
+
+        for( int i = 1; i < n_test_points; i++ )
+        {
+          double irrelevant_value = ( d_len - 1 ) * spacing + i / n_test_points * spacing;
+          std::vector< double > test_point( s, irrelevant_value );
+          test_point[ d ] = test_domain_value;
+
+          /* Captain! Test a point set here, after getting 1 to work */
+          double test_value = 
+          interpolate_basis_cell( basis_cell, test_point, d_len_s, max_value_s );
+
+          double check = std::abs( gold - test_value ) / test_value;
+          REQUIRE( check < 1e-10 );
+        }
+      }
+    }
+
+    //REQUIRE( false );
   }
 }
 
