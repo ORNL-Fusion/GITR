21ebf77e6516de464a53d707c67f14d76b11c31e
null <null>
2021-12-13 17:37:13 -0500

    moved anothe

--- include/data_file_io.h
+++ include/data_file_io.h
@@ -17,3 +17,74 @@ typedef float gitr_precision;
 int readFileDim(const std::string& fileName,const std::string& varName);
 
 int read_ar2Input( std::string fileName, gitr_precision *Bfield[]);
+
+template <typename T>
+int readFileVar(const std::string& fileName,const std::string& section,const std::string& varName,T &x ) {
+       std::string profiles_folder = "output/profiles";
+        // Check input file exists
+       std::ifstream file(fileName);
+       if(!file.good()) {
+         std::cout<<"ERROR: Cannot file input file ... "<<fileName<<std::endl;
+         exit(1);
+       }
+       else{
+         std::cout << "reading " <<" "  << fileName << std::endl;
+       }
+ 
+       netCDF::NcFile nc(fileName, netCDF::NcFile::read);
+
+       if(nc.isNull()){
+       std::cout << "ERROR: Failed to open " << fileName << std::endl; 
+       }
+
+       netCDF::NcVar xx;
+
+       try{
+           xx = nc.getVar(varName);
+           if(xx.isNull()){std::cout << "ERROR: could not find variable "<<
+              varName << " in " << fileName << std::endl;}
+       }
+       catch(netCDF::exceptions::NcException& e){}
+       
+       int numberOfDimensions = xx.getDimCount();
+       if(numberOfDimensions >1)
+       {
+           int nTotal = 1;
+           for(int j=0;j<numberOfDimensions;j++)
+           {
+               nTotal = nTotal*(xx.getDim(j)).getSize(); 
+           }
+           xx.getVar(&x);
+           if(numberOfDimensions == 2)
+           {
+            //OUTPUT2d(profiles_folder,section+varName+".m",
+            //        (xx.getDim(0)).getSize(), (xx.getDim(1)).getSize(),
+            //         &x[0]);
+           }
+           else if(numberOfDimensions ==3)
+           {
+            //OUTPUT3d(profiles_folder,section+varName+".m",
+            //        (xx.getDim(0)).getSize(), (xx.getDim(1)).getSize(), 
+            //        (xx.getDim(2)).getSize(), &x[0]);
+           }
+           return nTotal;
+       }
+       else
+       {
+         netCDF::NcDim xdim;
+       try{
+           xdim = xx.getDim(0);
+           if(xdim.isNull()){std::cout << "ERROR: could not get dimension of variable "<<
+              varName << " in " << fileName << std::endl;}
+       }
+       catch(netCDF::exceptions::NcException& e){}
+       int xlength;
+       xlength = xdim.getSize();
+
+       xx.getVar(&x);
+       std::string fullName = section+varName;
+       //OUTPUT1d(profiles_folder,fullName+".m", xlength, &x[0]);
+       nc.close();
+       return xlength;
+       } 
+}
--- include/utils.h
+++ include/utils.h
@@ -20,6 +20,8 @@
 #include <libconfig.h++>
 #include <fstream>
 #include "interp2d.hpp"
+#include "data_file_io.h"
+
 #if USE_DOUBLE
 typedef double gitr_precision;
 #else
@@ -53,77 +55,6 @@ extern template int get_variable(libconfig::Config &cfg, const std::string s);
 extern template float get_variable(libconfig::Config &cfg, const std::string s);
 extern template double get_variable(libconfig::Config &cfg, const std::string s);
 extern template const char* get_variable(libconfig::Config &cfg, const std::string s);
-
-template <typename T>
-int readFileVar(const std::string& fileName,const std::string& section,const std::string& varName,T &x ) {
-       std::string profiles_folder = "output/profiles";
-        // Check input file exists
-       std::ifstream file(fileName);
-       if(!file.good()) {
-         std::cout<<"ERROR: Cannot file input file ... "<<fileName<<std::endl;
-         exit(1);
-       }
-       else{
-         std::cout << "reading " <<" "  << fileName << std::endl;
-       }
- 
-       netCDF::NcFile nc(fileName, netCDF::NcFile::read);
-
-       if(nc.isNull()){
-       std::cout << "ERROR: Failed to open " << fileName << std::endl; 
-       }
-
-       netCDF::NcVar xx;
-
-       try{
-           xx = nc.getVar(varName);
-           if(xx.isNull()){std::cout << "ERROR: could not find variable "<<
-              varName << " in " << fileName << std::endl;}
-       }
-       catch(netCDF::exceptions::NcException& e){}
-       
-       int numberOfDimensions = xx.getDimCount();
-       if(numberOfDimensions >1)
-       {
-           int nTotal = 1;
-           for(int j=0;j<numberOfDimensions;j++)
-           {
-               nTotal = nTotal*(xx.getDim(j)).getSize(); 
-           }
-           xx.getVar(&x);
-           if(numberOfDimensions == 2)
-           {
-            //OUTPUT2d(profiles_folder,section+varName+".m",
-            //        (xx.getDim(0)).getSize(), (xx.getDim(1)).getSize(),
-            //         &x[0]);
-           }
-           else if(numberOfDimensions ==3)
-           {
-            //OUTPUT3d(profiles_folder,section+varName+".m",
-            //        (xx.getDim(0)).getSize(), (xx.getDim(1)).getSize(), 
-            //        (xx.getDim(2)).getSize(), &x[0]);
-           }
-           return nTotal;
-       }
-       else
-       {
-         netCDF::NcDim xdim;
-       try{
-           xdim = xx.getDim(0);
-           if(xdim.isNull()){std::cout << "ERROR: could not get dimension of variable "<<
-              varName << " in " << fileName << std::endl;}
-       }
-       catch(netCDF::exceptions::NcException& e){}
-       int xlength;
-       xlength = xdim.getSize();
-
-       xx.getVar(&x);
-       std::string fullName = section+varName;
-       //OUTPUT1d(profiles_folder,fullName+".m", xlength, &x[0]);
-       nc.close();
-       return xlength;
-       } 
-}
 
 template <typename T>
 int getVarFromFile (libconfig::Config &cfg,const std::string& file,const std::string& section,
