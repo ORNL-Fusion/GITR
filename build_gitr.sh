0460254d0f073ed06173dcd563caf8c4ba5ccda5
null <null>
2021-12-30 16:01:13 -0500

    match the cpc configuration from branch upgrade_build_system

--- build_gitr.sh
+++ build_gitr.sh
@@ -1,9 +1,5 @@
 cmake -S /home/5n4/GITR -B /home/5n4/build_gitr \
--DGITR_USE_CUDA=1 \
--DGITR_USE_NETCDF=1 \
--DGITR_USE_PRE_SHEATH_EFIELD=1 \
--DGITR_USE_SHEATH_EFIELD=0 \
 &> /home/5n4/build_gitr/build_log.txt ;
 
-cmake --build /home/5n4/build_gitr -- -j \
-&>> /home/5n4/build_gitr/build_log.txt
+#cmake --build /home/5n4/build_gitr -- -j \
+#&>> /home/5n4/build_gitr/build_log.txt
