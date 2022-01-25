ac4ef21f87fb04e019f10a546e95ca65453a6b95
null <null>
2022-01-25 18:08:26 -0500

    build script

--- build_gitr.sh
+++ build_gitr.sh
@@ -1,5 +1,5 @@
 cmake -S /home/5n4/GITR -B /home/5n4/build_gitr \
 &> /home/5n4/build_gitr/build_log.txt ;
 
-#cmake --build /home/5n4/build_gitr -- -j \
-#&>> /home/5n4/build_gitr/build_log.txt
+cmake --build /home/5n4/build_gitr -- -j \
+&>> /home/5n4/build_gitr/build_log.txt
