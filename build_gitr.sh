e9af5193fedbdb11479e34e447057d0306c34341
null <null>
2022-02-02 19:03:51 -0500

    updated build script to always use ninja

--- build_gitr.sh
+++ build_gitr.sh
@@ -1,7 +1,7 @@
-cmake -S /home/5n4/GITR -B /home/5n4/build_gitr -G Ninja \
+cmake -S $1 -B $2 -G Ninja \
 -DGITR_USE_CUDA=1 \
 -DGITR_USE_PRE_SHEATH_EFIELD=1 \
 -DGITR_USE_SHEATH_EFIELD=0 \
-&> /home/5n4/build_gitr/build_log.txt ;
+&> $2/cmake_output.txt ;
 
-cmake --build /home/5n4/build_gitr -- -j 0 &>> /home/5n4/build_gitr/build_log.txt
+cmake --build $2 -- -j 0 &> $2/build_output.txt
