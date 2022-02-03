56d6d967bf723bf6450c55b972c992ac841a23aa
null <null>
2022-02-02 19:26:38 -0500

    simplified build script

--- build_gitr.sh
+++ build_gitr.sh
@@ -1,7 +1,3 @@
-cmake -S $1 -B $2 -G Ninja \
--DGITR_USE_CUDA=1 \
--DGITR_USE_PRE_SHEATH_EFIELD=1 \
--DGITR_USE_SHEATH_EFIELD=0 \
-&> $2/cmake_output.txt ;
+cmake -S $1 -B $2 -G Ninja &> $2/cmake_output.txt ;
 
 cmake --build $2 -- -j 0 &> $2/build_output.txt
