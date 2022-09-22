9179b0810b400edb4bdac42ef4b839d106e2f281
hayes2419 <hayes@utk.edu>
2022-09-22 14:18:06 -0400

    removed the tab in user_options.cmake

--- CMake/user_options.cmake
+++ CMake/user_options.cmake
@@ -1,7 +1,7 @@
 # String description for each option
 set( description "(no description added - see define_options.cmake)" )
 set( GITR_USE_CUDA 0 CACHE STRING "${description}" FORCE )
-set( GITR_USE_OPENMP 0 CACHE STRING "${description}" FORCE )
+set( GITR_USE_OPENMP 1 CACHE STRING "${description}" FORCE )
 set( GITR_USE_MPI 0 CACHE STRING "${description}" FORCE )
 set( GITR_USE_DOUBLE 1 CACHE STRING "${description}" FORCE )
 
@@ -9,7 +9,7 @@ add_compile_definitions(
         USE_CUDA=${GITR_USE_CUDA}
         USE_MPI=${GITR_USE_MPI}
         USE_OPENMP=${GITR_USE_OPENMP}
-	      USE_DOUBLE=${GITR_USE_DOUBLE}
+	USE_DOUBLE=${GITR_USE_DOUBLE}
         LC_INTERP=0
         GENERATE_LC=0
         BIASED_SURFACE=0
