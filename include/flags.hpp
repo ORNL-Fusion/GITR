a3a4b14ed0d7bdafb2d7a48233256effed56b5e0
Tim <tyounkin19@gmail.com>
2023-04-18 10:30:03 -0400

    added support for default flags and filesystem for creating output directory

--- include/flags.hpp
+++ include/flags.hpp
@@ -20,10 +20,10 @@ class Flags : public ManagedAllocation
    const bool USE_SHEATH_DENSITY;
    CUDA_CALLABLE_MEMBER
    Flags(libconfig::Config &cfg) : 
-     USE_IONIZATION{initialize_flags(cfg,"USE_IONIZATION")},
-       FIXED_SEEDS{initialize_flags(cfg,"FIXED_SEEDS")},
-       USE_ADAPTIVE_DT{initialize_flags(cfg,"USE_ADAPTIVE_DT")},
-       USE_SHEATH_DENSITY{initialize_flags(cfg,"USE_SHEATH_DENSITY")} {};
-   bool initialize_flags(libconfig::Config &cfg, std::string s);
+     USE_IONIZATION{initialize_flags(cfg,"USE_IONIZATION",1)},
+       FIXED_SEEDS{initialize_flags(cfg,"FIXED_SEEDS",1)},
+       USE_ADAPTIVE_DT{initialize_flags(cfg,"USE_ADAPTIVE_DT",0)},
+       USE_SHEATH_DENSITY{initialize_flags(cfg,"USE_SHEATH_DENSITY",0)} {};
+   bool initialize_flags(libconfig::Config &cfg, std::string s, int default_value);
 };
 #endif
--- src/flags.cpp
+++ src/flags.cpp
@@ -8,10 +8,20 @@
 
 //CUDA_CALLABLE_MEMBER
 //Flags::Flags() : USE_IONIZATION{0} {};
-bool Flags::initialize_flags(libconfig::Config &cfg,std::string s) 
+bool Flags::initialize_flags(libconfig::Config &cfg,std::string s, int default_value) 
 {
   std::string base = "flags.";
-  int flag = getVariable_cfg<int> (cfg, base+s);
+  //int flag = getVariable_cfg<int> (cfg, base+s);
+  int flag;
+  if(cfg.lookupValue(base+s, flag))
+    {
+      std::cout << base+s << " = " << flag << std::endl;
+    }
+  else
+    {
+      flag = default_value;
+      std::cout << "WARNING: Failed importing " << base+s << ", defaulting value to "<< flag <<  std:: endl;
+    }
   if(flag > 0) return true;
   else return false;
 }
