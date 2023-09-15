f051e962639ca4454958512f5d6fcb638f4cfbca
Abdou Diaw <diawa@ornl.gov>
2023-09-15 16:15:19 -0400

    remove unnecessary file

--- include/thermalForce.h
+++ include/thermalForce.h
@@ -90,13 +90,9 @@ void operator()(std::size_t indx)  {
 	            dt_step = 0.0;
 		  }
                 }
-      // std:cout << " grad Ti interp " << std::endl;
-      interp2dVector(&gradTi[0], p->xprevious[indx], p->yprevious[indx], p->zprevious[indx], nR_gradT, nZ_gradT,
-                     gradTGridr, gradTGridz, gradTiR, gradTiZ, gradTiT, cylsymm );
-      //std::cout << "Position r z" << sqrt(p->xprevious*p->xprevious + p->yprevious*p->yprevious) << " " << p->zprevious << std::endl;
-      //std::cout << "grad Ti " << std::copysign(1.0,gradTi[0])*sqrt(gradTi[0]*gradTi[0] + gradTi[1]*gradTi[1]) << " " << gradTi[2] << std::endl;
-      interp2dVector(&gradTe[0], p->xprevious[indx], p->yprevious[indx], p->zprevious[indx], nR_gradT, nZ_gradT,
-                     gradTGridr, gradTGridz, gradTeR, gradTeZ, gradTeT, cylsymm );
+
+      interp2dVector(&gradTi[0], p->xprevious[indx], p->yprevious[indx], p->zprevious[indx], nR_gradT, nZ_gradT, gradTGridr, gradTGridz, gradTiR, gradTiZ, gradTiT, cylsymm );
+      interp2dVector(&gradTe[0], p->xprevious[indx], p->yprevious[indx], p->zprevious[indx], nR_gradT, nZ_gradT,gradTGridr, gradTGridz, gradTeR, gradTeZ, gradTeT, cylsymm );
       mu = p->amu[indx] / (background_amu + p->amu[indx]);
       alpha = p->charge[indx] * p->charge[indx] * 0.71;
       beta = 3 * (mu + 5 * std::sqrt(2.0) * p->charge[indx] * p->charge[indx] * (1.1 * std::pow(mu, (5 / 2)) - 0.35 * std::pow(mu, (3 / 2))) - 1) / (2.6 - 2 * mu + 5.4 * std::pow(mu, 2));
@@ -125,47 +121,16 @@ void operator()(std::size_t indx)  {
 	dv_ITGy = dv_ITG[1];
 	dv_ITGz = dv_ITG[2];
 
-    //std::cout << "mu " << mu << std::endl;
-    //std::cout << "alpha beta " << alpha << " " << beta << std::endl;
-    //std::cout << "ITG " << dv_ITG[0] << " " << dv_ITG[1] << " " << dv_ITG[2] << std::endl;
-    //std::cout << "gradTi " << gradTi[0] << " " << gradTi[1] << " " << gradTi[2] << std::endl;
-    //std::cout << "ETG " << dv_ETG[0] << " " << dv_ETG[1] << " " << dv_ETG[2] << std::endl;
-    //std::cout << "v before thermal force " << p->vx[indx] << " " << p->vy[indx] << " " << p->vz[indx] << std::endl;
-    /*
-    gitr_precision theta = atan2(p->yprevious,p->xprevious);
-    gitr_precision Ar = -1;
-    gitr_precision At = 0.0;
-    gitr_precision Az = 1;
-    gradTi[0] = cos(theta)*Ar - sin(theta)*At;
-    gradTi[1] = sin(theta)*Ar + cos(theta)*At;
-    gradTi[2] = Az;
-    */
     gitr_precision vx = p->vx[indx];
     gitr_precision vy = p->vy[indx];
     gitr_precision vz = p->vz[indx];
         vNorm = std::sqrt(vx*vx + vy*vy + vz*vz);
     p->vD[indx] = dv_ITG[2];    
-	//std::cout << "gradTi Parallel " << gradTiPar << std::endl;
-        //std::cout << "gradTi Parallel " << gradTi[0]<<gradTi[1]<<gradTi[2] << std::endl;
-        //p->vx[indx] = p->vx[indx] +dv_ITG[0];//alpha*(gradTe[0])   
-	//p->vy[indx] = p->vy[indx] +dv_ITG[1];//alpha*(gradTe[1])
-	//p->vz[indx] = p->vz[indx] +dv_ITG[2];//alpha*(gradTe[2])		
-        //vNorm2 = sqrt(p->vx[indx]*p->vx[indx] + p->vy[indx]*p->vy[indx] + p->vz[indx]*p->vz[indx]);
-		//SFT
         gitr_precision k1 = dv_ITG[2] - dt_step*p->nu_s[indx]
                     *(dv_ITG[2]);
         p->vx[indx] = vx + dv_ITG[0];///velocityCollisionsNorm;   	
 		p->vy[indx] = vy + dv_ITG[1];///velocityCollisionsNorm;   	
 		p->vz[indx] = vz + dv_ITG[2];// - dt*p->nu_s[indx]
-                         //*(0.5*k1);///velocityCollisionsNorm;   	
-        //p.vx = p.vx + (dt/(p.amu*MI))*(  beta*(gradTi[0]));//alpha*(gradTe[0])
-		//p.vy = p.vy + (dt/(p.amu*MI))*(  beta*(gradTi[1]));//alpha*(gradTe[1])
-		//p.vz = p.vz + (dt/(p.amu*MI))*(  beta*(gradTi[2]));//alpha*(gradTe[2])		
-     //   std::cout << "dv ion thermal x" << dt/(p.amu*MI)*(  beta*(gradTi[0])) << std::endl;	
-     //  std::cout << "dv ion thermal y" << dt/(p.amu*MI)*(  beta*(gradTi[1])) << std::endl;	
-     //  std::cout << "dv ion thermal z" << dt/(p.amu*MI)*(  beta*(gradTi[2])) << std::endl;	
-        //std::cout << "theta " << theta << std::endl;
-       //std::cout << "v after thermal force " << p.vx << " " << p.vy << " " << p.vz << std::endl;
         }
     	}
      
