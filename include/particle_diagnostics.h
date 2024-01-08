fe9fa62a3fe7fe1c3e8b34705a39b1d89711afc3
Younkin T R <tqd@fusiont5.ornl.gov>
2024-01-08 11:44:48 -0500

    example producing same results as postprocessing

---
+++ include/particle_diagnostics.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER_DEVICE __device__
+#define CUDA_CALLABLE_MEMBER_HOST __host__
+#else
+#define CUDA_CALLABLE_MEMBER_DEVICE
+#define CUDA_CALLABLE_MEMBER_HOST
+#endif
+
+#include "Particles.h"
+#include "Fields.h"
+#include "flags.hpp"
+#ifdef __CUDACC__
+#include <curand_kernel.h>
+#include <thrust/random.h>
+#endif
+
+#ifdef __GNUC__
+#include <random>
+#include <stdlib.h>
+#endif
+
+#if USE_DOUBLE
+typedef double gitr_precision;
+#else
+typedef float gitr_precision;
+#endif
+
+
+struct particle_diagnostics {
+  Flags *flags;
+  Particles *particlesPointer;
+  Boundary *boundaryVector;
+  bool times_logarithmic;
+  gitr_precision bin_edge_0_time;
+  gitr_precision bin_edge_1_time;
+  gitr_precision bin_edge_dt;
+  int n_bins_time;
+  gitr_precision *particle_time_histogram;
+  
+  particle_diagnostics(Flags *_flags, Particles *_particlesPointer, Boundary *_boundaryVector, bool _time_logarithmic,
+                       gitr_precision bin_edge_0_time, gitr_precision _bin_edge_1_time, gitr_precision _bin_edge_dt,
+                       int _n_bins_time, gitr_precision *_particle_time_histogram);
+
+  CUDA_CALLABLE_MEMBER_DEVICE
+  void operator()(std::size_t indx);
+};
---
+++ src/particle_diagnostics.cpp
@@ -0,0 +1,50 @@
+#include "particle_diagnostics.h"
+#include "spectroscopy.h"
+
+
+particle_diagnostics::particle_diagnostics(Flags *_flags,
+          Particles *_particlesPointer,
+          Boundary *_boundaryVector,
+          bool _times_logarithmic,
+          gitr_precision _bin_edge_0_time,
+          gitr_precision _bin_edge_1_time,
+          gitr_precision _bin_edge_dt,
+          int _n_bins_time,
+          gitr_precision *_particle_time_histogram )
+      
+        :
+
+        flags(_flags), particlesPointer(_particlesPointer), boundaryVector(_boundaryVector),  times_logarithmic(_times_logarithmic),
+                       bin_edge_0_time(_bin_edge_0_time), bin_edge_1_time(_bin_edge_1_time), bin_edge_dt(_bin_edge_dt),
+                       n_bins_time(_n_bins_time), particle_time_histogram(_particle_time_histogram)
+{ }
+
+CUDA_CALLABLE_MEMBER_DEVICE
+void particle_diagnostics::operator()(std::size_t indx)
+{
+  if (particlesPointer->hitWall[indx] == 1.0) 
+  {
+    int wallHit = particlesPointer->surfaceHit[indx];
+    int surfaceHit = boundaryVector[wallHit].surfaceNumber;
+
+    gitr_precision p_time = particlesPointer->time[indx] - particlesPointer->transitTime[indx];
+    particlesPointer->transitTime[indx] = particlesPointer->time[indx];
+
+    int ind_time = std::floor((std::log10(p_time) - bin_edge_0_time)/bin_edge_dt);
+
+    int ind_2d = surfaceHit*n_bins_time + ind_time;
+
+    if (ind_time >=0 && ind_time < n_bins_time)
+    {
+             #if USE_CUDA > 0
+               atomicAdd1(&particle_time_histogram[ind_2d],particlesPointer->weight[indx]);
+             #else      
+               #pragma omp atomic
+               particle_time_histogram[ind_2d] = particle_time_histogram[ind_2d] + particlesPointer->weight[indx];
+             #endif
+
+    }
+  }
+
+}
+
