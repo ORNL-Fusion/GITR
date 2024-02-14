c57299c0701d5149bc3f481c96e466a649429a41
Alyssa Hayes <hayes@utk.edu>
2024-02-14 11:49:57 -0500

    Update particle_diagnostics.h to add in *_particle_angle_histogram

--- include/particle_diagnostics.h
+++ include/particle_diagnostics.h
@@ -36,17 +36,19 @@ struct particle_diagnostics {
   gitr_precision bin_edge_0_time;
   gitr_precision bin_edge_1_time;
   gitr_precision bin_edge_dt;
+  gitr_precision *particle_time_histogram;
   int n_bins_time;
   gitr_precision bin_edge_0_angle;
   gitr_precision bin_edge_1_angle;
   gitr_precision bin_edge_dtheta;
   int n_bins_angle;
-  gitr_precision *particle_time_histogram;
+  gitr_precision *particle_angle_histogram;
   
   particle_diagnostics(Flags *_flags, Particles *_particlesPointer, Boundary *_boundaryVector, bool _time_logarithmic,
                        gitr_precision _bin_edge_0_time, gitr_precision _bin_edge_1_time, gitr_precision _bin_edge_dt,
-                       int _n_bins_time, gitr_precision _bin_edge_0_angle, gitr_precision _bin_edge_1_angle, 
-                       gitr_precision _bin_edge_dtheta, int _n_bins_angle, gitr_precision *_particle_time_histogram);
+                       int _n_bins_time, gitr_precision *_particle_time_histogram, gitr_precision _bin_edge_0_angle, 
+                       gitr_precision _bin_edge_1_angle, gitr_precision _bin_edge_dtheta, int _n_bins_angle, 
+                       gitr_precision *_particle_angle_histogram);
 
   CUDA_CALLABLE_MEMBER_DEVICE
   void operator()(std::size_t indx);
