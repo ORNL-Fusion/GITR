df24c9148ab8b5406c74fb036070839da917119f
Alyssa Hayes <hayes@utk.edu>
2024-03-05 17:16:13 -0500

    Update particle_diagnostics.h to add log(angle) capability

--- include/particle_diagnostics.h
+++ include/particle_diagnostics.h
@@ -36,8 +36,9 @@ struct particle_diagnostics {
   gitr_precision bin_edge_0_time;
   gitr_precision bin_edge_1_time;
   gitr_precision bin_edge_dt;
-  gitr_precision *particle_time_histogram;
   int n_bins_time;
+  gitr_precision *particle_time_histogram;
+  bool angle_logarithmic
   gitr_precision bin_edge_0_angle;
   gitr_precision bin_edge_1_angle;
   gitr_precision bin_edge_dtheta;
@@ -46,9 +47,9 @@ struct particle_diagnostics {
   
   particle_diagnostics(Flags *_flags, Particles *_particlesPointer, Boundary *_boundaryVector, bool _time_logarithmic,
                        gitr_precision _bin_edge_0_time, gitr_precision _bin_edge_1_time, gitr_precision _bin_edge_dt,
-                       int _n_bins_time, gitr_precision *_particle_time_histogram, gitr_precision _bin_edge_0_angle, 
-                       gitr_precision _bin_edge_1_angle, gitr_precision _bin_edge_dtheta, int _n_bins_angle, 
-                       gitr_precision *_particle_angle_histogram);
+                       int _n_bins_time, gitr_precision *_particle_time_histogram, bool _angle_logarithmic, 
+                       gitr_precision _bin_edge_0_angle, gitr_precision _bin_edge_1_angle, gitr_precision _bin_edge_dtheta, 
+                       int _n_bins_angle, gitr_precision *_particle_angle_histogram);
 
   CUDA_CALLABLE_MEMBER_DEVICE
   void operator()(std::size_t indx);
