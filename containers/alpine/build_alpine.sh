58b23b094e79ec03017f9aa5121e509ea3fa90ce
Harry Hughes <harrnh3@gmail.com>
2024-06-20 16:43:39 -0400

    added interactive alpine container

--- containers/alpine/build_alpine.sh
+++
@@ -1,4 +0,0 @@
-#DOCKER_BUILDKIT=1 \
-docker build \
---file containers/Dockerfile_alpine \
---tag alpine_interactive_image .
--- containers/run_alpine.sh
+++ containers/run_alpine.sh
@@ -1,4 +1,4 @@
 docker run -i -t --rm \
 --name alpine_interactive_container \
 -v $(pwd):/host \
-alpine_interactive_image
+stonecoldhughes/gitr:cpu_gitr_alpine_interactive
