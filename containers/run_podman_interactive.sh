f597ed5d830d9808dc4e8738349ecc41cbe08f36
hayes2419 <hayes@utk.edu>
2024-07-15 11:02:59 -0700

    add volume lines to interactive podman build bash script

--- containers/run_podman_interactive.sh
+++ containers/run_podman_interactive.sh
@@ -2,4 +2,7 @@ podman-hpc run -i -t --rm \
 --name gitr_interactive \
 --gpu \
 -v $(pwd):/host \
+--volume="/etc/group:/etc/group:ro" \
+--volume="/etc/passwd:/etc/passwd:ro" \
+--volume="/etc/shadow:/etc/shadow:ro" \
 docker.io/stonecoldhughes/gitr:gpu_gitr_interactive
