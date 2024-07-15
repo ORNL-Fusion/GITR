6852064fa4c80f163e916ed8c07d371703a78b66
hayes2419 <hayes@utk.edu>
2024-07-15 12:09:47 -0700

    add untested noninteractive podman bash script

--- containers/run_podman_noninteractive.sh
+++ containers/run_podman_noninteractive.sh
@@ -1,5 +1,8 @@
 podman-hpc run -i -t --rm \
---name gitr_container \
+--name gitr_interactive \
 --gpu \
 -v $(pwd):/host \
-docker.io/stonecoldhughes/gitr:noninteractive
+--volume="/etc/group:/etc/group:ro" \
+--volume="/etc/passwd:/etc/passwd:ro" \
+--volume="/etc/shadow:/etc/shadow:ro" \
+docker.io/stonecoldhughes/gitr:gpu_gitr_noninteractive
